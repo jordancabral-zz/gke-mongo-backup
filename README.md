a4992b655df08a650644d522b86253aa6a0e9fe6
Jordan Cabral <jordan.cabral@wenance.com>
2018-07-12 12:01:57 -0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# gke-mongo-backup
+# GKE mongo backup
 Scripts and config to backup mongodb to Google Cloud Storage in GKE
+
+## Contains:
+- kubernetes cronJob to execute our docker image with the necesari script and dependencies
+- Dockerfile of the image used by de cronJob
+- Script with the necesary steps to backup database and upload to GCS
+
+## Steps
+- Create bucket in Google Cloud Storage to upload backups
+
+- Create Google cloud service account with credentials and access permisions for GCS (Storage Object Creator, Storage Object Viewer): 
+https://cloud.google.com/kubernetes-engine/docs/tutorials/authenticating-to-cloud-platform?hl=es
+
+- Create kubernetes secret in your kubernetes cluster to store Service Account Credentials:
+  kubectl create secret generic gcs-key --from-file=key.json=<PATH-TO-KEY-FILE>.json
+
+- Build and upload docker image to your registry
+
+- Apply cron.yaml to your kubernetes cluster:
+  kubectl apply -f cron.yaml
+
+## Local test:
+- Need to save your Google cloud service account credentials json in same path 
+- Run:
+docker run -ti -v .:/var/mongobackup -e GOOGLE_APPLICATION_CREDENTIALS='/var/mongobackup/<CREDENTIALS_FILENAME>.json' -e MONGO_URI='<MONGO_URI>' -e BUCKET_NAME='<BUCKET_NAME>' mongobackup
---
+++ mongobackup.sh
@@ -0,0 +1,50 @@
+# Utility functions
+get_log_date () {
+    date +[%Y-%m-%d\ %H:%M:%S]
+}
+get_file_date () {
+    date +%Y%m%d%H%M%S
+}
+
+# Validate needed ENV vars
+if [ -z "$MONGO_URI" ]; then
+    echo "$(get_log_date) MONGO_URI is unset or set to the empty string"
+    exit 1
+fi
+if [ -z "$BUCKET_NAME" ]; then
+    echo "$(get_log_date) BUCKET_NAME is unset or set to the empty string"
+    exit 1
+fi
+
+# Path in which to create the backup (will get cleaned later)
+BACKUP_PATH="/mnt/data/dump/"
+
+# START
+echo "$(get_log_date) Mongo backup started"
+
+# Activate google cloud service account
+echo "$(get_log_date) Activating service account"
+gcloud auth activate-service-account --key-file=$GOOGLE_APPLICATION_CREDENTIALS
+
+# Backup filename
+BACKUP_FILENAME="$(get_file_date).tar.gz"
+
+# Create the backup
+echo "$(get_log_date) [Step 1/3] Running mongodump from $MONGO_URI to $BACKUP_PATH"
+mongodump --uri $MONGO_URI -o $BACKUP_PATH --quiet
+
+# Compress
+echo "$(get_log_date) [Step 2/3] Creating tar file"
+tar -czf $BACKUP_PATH$BACKUP_FILENAME *
+
+# Copy to Google Cloud Storage
+echo "$(get_log_date) [Step 3/3] Uploading archive to Google Cloud Storage"
+echo "Copying $BACKUP_PATH$BACKUP_FILENAME to gs://$BUCKET_NAME/$BACKUP_FILENAME"
+gsutil cp $BACKUP_PATH$BACKUP_FILENAME gs://$BUCKET_NAME/$BACKUP_FILENAME 2>&1
+
+# Clean
+echo "Removing backup data"
+rm -rf $BACKUP_PATH*
+
+# FINISH
+echo "$(get_log_date) Copying finished"
